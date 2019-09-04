c35172b9fba671c8308e6bc4856cf12c9d88e2a5
[PengZuo] <[zp_crash@163.com]>
2019-09-04 16:04:48 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-ZPQRCodePod_Example/Pods-ZPQRCodePod_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZPQRCodePod_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ZPQRCodePod_Example
+@end
---
+++ Example/Pods/Target Support Files/ZPQRCodePod/ZPQRCodePod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZPQRCodePod : NSObject
+@end
+@implementation PodsDummy_ZPQRCodePod
+@end
